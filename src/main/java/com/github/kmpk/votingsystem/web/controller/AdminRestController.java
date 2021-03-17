c94345a0aab8a83d93ca8344b811d5e12d984943
kmpk <kmpk.gm@gmail.com>
2021-03-17 23:51:02 +0300

    Add logging

--- src/main/java/com/github/kmpk/votingsystem/web/controller/AdminRestController.java
+++ src/main/java/com/github/kmpk/votingsystem/web/controller/AdminRestController.java
@@ -1,11 +1,11 @@
 package com.github.kmpk.votingsystem.web.controller;
 
-import com.github.kmpk.votingsystem.AuthorizedUser;
 import com.github.kmpk.votingsystem.model.User;
 import com.github.kmpk.votingsystem.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,13 +14,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController()
 @RequestMapping(AdminRestController.REST_URL)
 public class AdminRestController {
+    private static final Logger logger = LoggerFactory.getLogger(AdminRestController.class);
+
     static final String REST_URL = "/rest/admin/users";
 
     @Autowired
     private UserService service;
 
-    @GetMapping(path = "/{id}",produces = MediaType.APPLICATION_JSON_VALUE)
-    public User get(@AuthenticationPrincipal AuthorizedUser authUser,@PathVariable("id") int id) {
+    @GetMapping(path = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public User get(@PathVariable("id") int id) {
         return service.get(id);
     }
 }
--- src/main/java/com/github/kmpk/votingsystem/web/controller/ProfileRestController.java
+++ src/main/java/com/github/kmpk/votingsystem/web/controller/ProfileRestController.java
@@ -5,6 +5,8 @@ import com.github.kmpk.votingsystem.model.User;
 import com.github.kmpk.votingsystem.service.UserService;
 import com.github.kmpk.votingsystem.to.UserTo;
 import com.github.kmpk.votingsystem.util.ValidationUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -19,6 +21,8 @@ import java.net.URI;
 @RestController()
 @RequestMapping(ProfileRestController.REST_URL)
 public class ProfileRestController {
+    private static final Logger logger = LoggerFactory.getLogger(ProfileRestController.class);
+
     static final String REST_URL = "/rest/profile";
 
     @Autowired
@@ -26,18 +30,21 @@ public class ProfileRestController {
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public User get(@AuthenticationPrincipal AuthorizedUser authUser) {
+        logger.info("get by user {}", authUser.getId());
         return service.get(authUser.getId());
     }
 
     @DeleteMapping
     @ResponseStatus(value = HttpStatus.NO_CONTENT)
     public void delete(@AuthenticationPrincipal AuthorizedUser authUser) {
+        logger.info("delete by user {}", authUser.getId());
         service.delete(authUser.getId());
     }
 
     @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(value = HttpStatus.NO_CONTENT)
     public void update(@Valid @RequestBody UserTo userTo, @AuthenticationPrincipal AuthorizedUser authUser) {
+        logger.info("update by user {}", authUser.getId());
         ValidationUtil.assureIdConsistent(userTo, authUser.getId());
         service.update(userTo);
     }
@@ -45,6 +52,7 @@ public class ProfileRestController {
     @PostMapping(value = "/register", consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(value = HttpStatus.CREATED)
     public ResponseEntity<User> register(@Valid @RequestBody UserTo userTo) {
+        logger.info("register new user {}", userTo.getEmail());
         User created = service.create(new User(userTo.getName(), userTo.getEmail(), userTo.getPassword()));
         URI uriOfNewResource = ServletUriComponentsBuilder.fromCurrentContextPath()
                 .path(REST_URL + "/{id}")
